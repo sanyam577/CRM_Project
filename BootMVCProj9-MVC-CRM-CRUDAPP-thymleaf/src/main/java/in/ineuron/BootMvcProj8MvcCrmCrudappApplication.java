8db9bb91a5c97236b2b941059b0ec75f598e4904
Sanyam <singhsanyam577@gmail.com>
2023-06-07 18:17:33 +0530

    First Commit

---
+++ BootMVCProj9-MVC-CRM-CRUDAPP-thymleaf/src/main/java/in/ineuron/BootMvcProj8MvcCrmCrudappApplication.java
@@ -0,0 +1,15 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj8MvcCrmCrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj8MvcCrmCrudappApplication.class, args);
+	}
+
+}
+
+//url:- http://localhost:9999/customer/list
